7ab3e7a1d5ba2fff61a94bb64839c1e72d8c4844
bcokca <bcokca@gmail.com>
2014-06-15 23:33:29 -0700

    configuration for firebase url

---
+++ web-app/scripts/configuration/config.js
@@ -0,0 +1,3 @@
+firebase = {
+    url : "boiling-fire-2567"//change this with your own
+};
--- web-app/scripts/services/api.js
+++ web-app/scripts/services/api.js
@@ -5,13 +5,12 @@
 
 
 //services come here
-
 var fireBaseServices = angular.module('fireBaseServices', [], function($provide){
 
     $provide.factory("loginService", function($firebase, $firebaseSimpleLogin) {
         return {
             getAuth :  function(){
-                var ref = new Firebase("https://boiling-fire-2567.firebaseio.com");
+                var ref = new Firebase("https://" + firebase.url + ".firebaseio.com");
                 return $firebaseSimpleLogin(ref);
             }
         };
@@ -20,7 +19,7 @@ var fireBaseServices = angular.module('fireBaseServices', [], function($provide)
 
     $provide.factory("chatService", function($firebase) {
 
-        var ref = new Firebase("https://boiling-fire-2567.firebaseio.com/chat");
+        var ref = new Firebase("https://" + firebase.url + ".firebaseio.com/chat");
         return $firebase(ref);
 
     });
