b8d27fe486fe28c199f164729ddfa9911fe6a31a
bcokca <bcokca@gmail.com>
2014-06-15 18:42:34 -0700

    angularfire added

--- web-app/scripts/controllers/controllers.js
+++ web-app/scripts/controllers/controllers.js
@@ -1,43 +1,9 @@
-angularFireApp.controller('AppCtrl', function AppCtrl($scope, $location, $rootScope, $http, $firebase, $firebaseSimpleLogin) {
+angularFireApp.controller('AppCtrl', function AppCtrl($scope, $location, $rootScope,
+                                                      $http, $firebase, $firebaseSimpleLogin) {
 
     //firebase logggin with facebook
-    $scope.chatRef = new Firebase('https://boiling-fire-2567.firebaseio.com');
-    $scope.auth = new FirebaseSimpleLogin($scope.chatRef, function(error, user) {
-        if (error) {
-            // an error occurred while attempting login
-            console.log(error);
-        } else if (user) {
-            // user authenticated with Firebase
-            console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
-            $rootScope.currentUser = user;
-
-            $location.path('/main');
-
-        } else {
-            // user is logged out
-        }
-    });
-
-    $scope.loginUser = function(provider){
-        $scope.auth.login(provider, {
-            rememberMe: true,
-            scope: 'email, user_likes'
-        });
-        console.log('user logged in');
-
-    };
-
-    $scope.logoutUser = function(){
-        $scope.auth.logout();
-        $location.path('/radio');
-        console.log('user logout');
-    };
-
-
-
-
-
-
+    var ref = new Firebase('https://boiling-fire-2567.firebaseio.com');
+    $scope.auth = $firebaseSimpleLogin(ref);
 
 
 });
