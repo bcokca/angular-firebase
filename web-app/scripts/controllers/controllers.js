b7993bd144f46d85c61910531cd5f16acff41d08
bcokca <bcokca@gmail.com>
2014-06-15 20:42:20 -0700

    refactoring chat module

--- web-app/scripts/controllers/controllers.js
+++ web-app/scripts/controllers/controllers.js
@@ -5,4 +5,15 @@ angularFireApp.controller('AppCtrl', function AppCtrl($scope, $location, $rootSc
     //var ref = new Firebase('https://boiling-fire-2567.firebaseio.com');
     $rootScope.auth = loginService.getAuth();
 
+    $scope.logout = function(){
+        $rootScope.auth.$logout();
+        $rootScope.auth.user = null;
+        $location.path('/login');
+    };
+
+    $scope.login = function(provider){
+        $rootScope.auth.$login(provider);
+
+    }
+
 });
