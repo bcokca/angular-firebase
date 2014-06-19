96b68eda689cb7e92512af73463e209e600788f1
bcokca <bcokca@gmail.com>
2014-06-19 01:17:21 -0700

    twitter support added for login

--- web-app/scripts/controllers/chat.js
+++ web-app/scripts/controllers/chat.js
@@ -5,9 +5,12 @@
 angularFireApp.controller('ChatCtrl', function AppCtrl($scope, $location, $rootScope,chatService) {
 
     $scope.user = $rootScope.auth.user.displayName;
+    if($rootScope.auth.user.provider == 'twitter');{
+        $scope.image_url = $rootScope.auth.user.thirdPartyUserData.profile_image_url;
+    }
     $scope.messages = chatService;
     $scope.addMessage = function() {
-        $scope.messages.$add({from: $scope.user, content: $scope.message});
+        $scope.messages.$add({from: $scope.user, content: $scope.message, img_url: $scope.image_url});
         $scope.message = "";
     };
 
